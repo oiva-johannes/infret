2da55804f56a913f9b00af36fe94031ea902b5c7
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-01-24 02:56:57 +0200

    #2: remove duplicate headlines & start utilizing news_object module

--- web_scraping.py
+++ web_scraping.py
@@ -1,19 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import news_object
 
-try:
-    source = requests.get("https://yle.fi/")
-    source.raise_for_status() #Raises an error if there is a problem with the url
-    
-    soup = BeautifulSoup(source.text, "html.parser")
-    headlines = soup.find_all("h3") #Find all h3 headers 
-    
-    word = "Venä[jä|l]"
-    
-    for headline in headlines:
-        if re.search(fr"\b{word}\w*\b", headline.text): #Matches all the headlines with the specified word
-            print(headline.text)
-   
-except Exception as e:
-    print(e)
+
+def ScrapeYle():
+
+    try:
+        source = requests.get("https://yle.fi/")
+        source.raise_for_status() #Raises an error if there is a problem with the url
+        
+        soup = BeautifulSoup(source.text, "html.parser")
+        headlines = soup.find_all(re.compile('h[1-6]')) #Find all the headers 
+        
+        word = "Venä[jä|l]"
+        already_seen = set()
+
+        for headline in headlines:
+            if re.search(fr"\b{word}\w*\b", headline.text): #Matches all the headlines with the specified word
+                if headline.text not in already_seen:
+                    already_seen.add(headline.text)
+                    link = "https://yle.fi" + re.findall("/[a-z]/[0-9][0-9]-[0-9]+", str(headline))[0]
+                    header_size = re.findall("h[1-6]", str(headline))[0]
+                    news_object.NewsArticle(headline.text, link, header_size)
+
+    except Exception as e:
+        print(e)
+
+
+ScrapeYle()
+news_object.NewsArticle.show()

fdc18b1efbbaa639f975bbce6a19329bb2af0d1e
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-01-24 02:55:39 +0200

    #2: add object structure for articles

---
+++ news_object.py
@@ -0,0 +1,16 @@
+class NewsArticle:
+    __articles = []
+
+    def __init__(self, text, link, header):
+        self.__text = text
+        self.__link = link
+        self.__header = header
+        self.__articles.append(self)
+    
+    def __str__(self) -> str:
+        return f"\nText: {self.__text}\nLink: {self.__link}\nHeader size: {self.__header}\n"
+                
+    @staticmethod
+    def show():
+        for article in NewsArticle.__articles:
+            print(article)

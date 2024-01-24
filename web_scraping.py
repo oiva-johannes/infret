ee1f0d22dfaf0db96b23a55cbcf25204004bc1ff
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-01-24 03:38:20 +0200

    ticket #2: refactor function structures

--- web_scraping.py
+++ web_scraping.py
@@ -4,15 +4,15 @@ import re
 import news_object
 
 
-def ScrapeYle():
+def ScrapeNews(news_site: dict):
 
     try:
-        source = requests.get("https://yle.fi/")
+        source = requests.get(news_site['link'])
         source.raise_for_status() #Raises an error if there is a problem with the url
-        
+
         soup = BeautifulSoup(source.text, "html.parser")
         headlines = soup.find_all(re.compile('h[1-6]')) #Find all the headers 
-        
+
         word = "Venä[jä|l]"
         already_seen = set()
 
@@ -20,13 +20,22 @@ def ScrapeYle():
             if re.search(fr"\b{word}\w*\b", headline.text): #Matches all the headlines with the specified word
                 if headline.text not in already_seen:
                     already_seen.add(headline.text)
-                    link = "https://yle.fi" + re.findall("/[a-z]/[0-9][0-9]-[0-9]+", str(headline))[0]
+                    article_link = news_site['link'] + re.findall("/[a-z]/[0-9][0-9]-[0-9]+", str(headline))[0]
                     header_size = re.findall("h[1-6]", str(headline))[0]
-                    news_object.NewsArticle(headline.text, link, header_size)
+                    news_object.NewsArticle(headline.text, article_link, header_size, news_site['provider'])
 
     except Exception as e:
         print(e)
 
 
-ScrapeYle()
-news_object.NewsArticle.show()
+def main():
+
+    Yle = {'link': 'https://yle.fi',
+           'provider': 'Yle'}
+
+    ScrapeNews(Yle)
+    news_object.NewsArticle.show()
+
+
+if __name__ == "__main__":
+    main()

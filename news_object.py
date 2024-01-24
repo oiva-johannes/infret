466a7e39bff4574b36c588606a26c4327b98f511
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-01-24 03:31:28 +0200

    add news provider attribute

--- news_object.py
+++ news_object.py
@@ -1,10 +1,11 @@
 class NewsArticle:
     __articles = []
 
-    def __init__(self, text, link, header):
+    def __init__(self, text, link, header, provider):
         self.__text = text
         self.__link = link
         self.__header = header
+        self.__provider = provider
         self.__articles.append(self)
     
     def __str__(self) -> str:

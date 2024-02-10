a3a9779094e624e253e954cdb4b72e58b905f330
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-02-10 18:43:10 +0200

    improve stemming, query handling & code structure

--- cosine_search.py
+++ cosine_search.py
@@ -1,11 +1,13 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import re
+import nltk
 from nltk.tokenize import word_tokenize
-from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.snowball import FinnishStemmer
+from scipy.sparse import vstack, hstack
 
-stemmer = SnowballStemmer("finnish") # a stemmer for finnish
 
+stemmer = FinnishStemmer() # a stemmer for finnish
 
 def read_data(f_path: str):
 
@@ -17,46 +19,57 @@ def read_data(f_path: str):
 
 
 def search_documents(query: str, documents: str):
-    
-    if query[0] == '"' and query[-1] == '"': # checks if the query has double quotes
-        query = query[1:-1] # removes the double quotes from the query
-        tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
-        sparse_matrix = tfv.fit_transform(documents).T.tocsr() # search the match from the unstemmed documents
-        query_vec = tfv.transform([query]).tocsc() # use the unstemmed query
-    
-    else: # if the query is not enclosed in double quotes, search for all matches for the stem
-        
-        stemmed_query = stemmer.stem(query) # stem the query 
-        print(stemmed_query) # debug print
-    
-        stemmed_documents = stem_documents(documents) # stem also the documents so the documents and the query match
-    
-        tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
-        sparse_matrix = tfv.fit_transform(stemmed_documents).T.tocsr() # use stemmed documents
-        query_vec = tfv.transform([stemmed_query]).tocsc() # also the stemmed query
 
-    hits = np.dot(query_vec, sparse_matrix)
+    tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+    query = query.split(" ")
+    print(query)
+
+    stemmed_documents = stem_documents(documents) # stem also the documents so the documents and the query match
+    arrays = []
+
+    for q in query:
+
+        q = q.lower()
+        if q[0] == '"' and q[-1] == '"': # checks if the query has double quotes
+            q = q[1:-1] # removes the double quotes from the query
+            print(q) # debug
+            sparse_matrix = tfv.fit_transform(documents).T.tocsr()
+            query_vec = tfv.transform([q]).tocsc()
+            hits = np.dot(query_vec, sparse_matrix)
+            arrays.append(hits)
+
+        else: # if the word is not enclosed in double quotes, search for all matches for the stem    
+            stemmed_q = stemmer.stem(q) # stem the query
+            print(stemmed_q) # debug
+            sparse_matrix = tfv.fit_transform(stemmed_documents).T.tocsr()
+            query_vec = tfv.transform([stemmed_q]).tocsc()
+            hits = np.dot(query_vec, sparse_matrix)
+            arrays.append(hits)
+
+    hits = vstack(arrays)
+    hits = hits.tocsc()
     if hits.nnz > 0: # check that the number of matches is positive
         ranked = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
-        print(f'\n\nThe search found {len(ranked)} document matches for your query: "{query}", with varying precisions. Here they are ranked in order from best to worst:\n\n')
+        already_seen = set()
+        ranked = [r for r in ranked if r[1] not in already_seen and not already_seen.add(r[1])]
 
-        for i, article in enumerate(ranked):
-            score = article[0]
-            document = documents[article[1]]
-            header = re.findall(r"name.*[0-9]", document)[0]
-            print(f"Match #{i + 1} with the score of {score}:\n{header}\n")
+        return ranked
+    
     else:
-        print(f'\n\nThe search did not find any document to match your query: "{query}"\n\n')
+        return None
+
 
 def stem_documents(documents): # tokenize all the documents and stem them
+    
     stem_documents = []
     for document in documents:
-        words = word_tokenize(document)
+        words = word_tokenize(document, language='finnish')
         stem_words = [stemmer.stem(word) for word in words]
         stem_document = ' '.join(stem_words)
         stem_documents.append(stem_document)
     
     return stem_documents
+
 
 def main():
 
@@ -67,7 +80,18 @@ def main():
         if query == "quit":
              break
                 
-        search_documents(query, documents)
+        ranked = search_documents(query, documents)
+
+        if ranked:
+            print(f"\n\nThe search found {len(ranked)} document matches for your query: '{query}', with varying precisions. Here they are ranked in order from best to worst:\n\n")
+    
+            for i, article in enumerate(ranked):
+                score = article[0]
+                document = documents[article[1]]
+                header = re.findall(r"name.*[0-9]", document)[0]
+                print(f"Match #{i + 1} with the score of {score}:\n{header}\n")
+        else:
+            print(f"\n\nThe search did not find any document to match your query: '{query}'\n\n")
 
 
 if __name__ == "__main__":

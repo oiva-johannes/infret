cb802712c768bca785c43e948d2bdd631aaa65ef
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-02-23 08:34:38 +0200

    add check for duplicate articles & remove unnecessary data columns

--- web_scraping.py
+++ web_scraping.py
@@ -2,14 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
-import os
-import datetime
-
-if os.path.isfile("./dynamic-datasets/articles_excel.xlsx"):
-    os.remove("./dynamic-datasets/articles_excel.xlsx")
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from dateutil import parser
 
 
 def ScrapeYle(articles: list[pd.DataFrame]):
+
+    old_links = []
+    if articles:
+        old_links = articles[0].to_dict('list')['href']
 
     source = requests.get("https://yle.fi")
     source.raise_for_status() #Raises an error if there is a problem with the url
@@ -25,6 +27,7 @@ def ScrapeYle(articles: list[pd.DataFrame]):
             
             href = re.findall('href.*\">', str(headline))[0]
             if "https://areena.yle.fi" in href:
+                print(None, "wrong type")
                 continue
             
             if "https://yle.fi" not in href:
@@ -32,16 +35,20 @@ def ScrapeYle(articles: list[pd.DataFrame]):
             else:
                 href = href[6:-2]
 
-            print(href) # debug print    
-
+            if href in old_links:
+                print(None, "old href")
+                continue
+            
             source_article = requests.get(href)
             source_article.raise_for_status()
             bsoup = BeautifulSoup(source_article.text, "html.parser")
 
             content = bsoup.find('section', class_="yle__article__content")
             if content == None or content.text == None or content.text == "" or content.text == [] or content == "":
-                print(None, "skipped")  # debug print
+                print(None, "content")  # debug print
                 continue
+
+            print(href) # debug print
 
             rege = re.compile('.*yle__article__paragraph')
             p = bsoup.find(class_=rege)
@@ -54,27 +61,28 @@ def ScrapeYle(articles: list[pd.DataFrame]):
             date = re.findall('datetime.*">', str(time))
             if len(date) != 0:
                 date = date[0][10:-2]
+                dt_object = parser.parse(date)
+                fin_zone = dt_object.astimezone(ZoneInfo("Europe/Helsinki"))
+                date = fin_zone.strftime('%d.%m.%Y')
             else:
-                date = datetime.datetime.now().strftime('%Y-%m-%d')
+                fin_zone = datetime.now(ZoneInfo("Europe/Helsinki"))
+                date = fin_zone.strftime('%d.%m.%Y')
 
 
             text = content.text
             popularity = index+1
             provider = "Yle"
             header = headline.text
-            article_id = re.findall("/a/.*", href)[0][3:]
-            article_id = article_id.replace("-", "")
-    
+
 
             df = pd.DataFrame({
                 "popularity": popularity,
                 "header": header,
                 "href": href,
                 "date": date,
-                "articleid": article_id,
                 "provider": provider,
                 "paragraph": paragraph,
-                "text": text,}, index=[len(articles)])
+                "text": text,}, index=[len(articles)+1])
             
             articles.append(df)
  
@@ -82,9 +90,17 @@ def ScrapeYle(articles: list[pd.DataFrame]):
 def main():
 
     articles = []
+    try:
+        df_ex = pd.read_excel('dynamic-datasets/articles_excel.xlsx')
+        articles.append(df_ex)
+    except:
+        print("no previous articles\n")
+
     ScrapeYle(articles)
     df = pd.concat(articles)
-    df.to_excel('dynamic-datasets/articles_excel.xlsx', index_label="indexdf")
+
+    sorted_df = df.sort_values(by='popularity')
+    sorted_df.to_excel('dynamic-datasets/articles_excel.xlsx', index=False)
     #df.to_parquet('dynamic-datasets/articles.parquet')
 
 

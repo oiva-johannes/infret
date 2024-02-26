6a0bf5a8c488231675dfb828333661756213fce3
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-02-26 02:40:31 +0200

    handle empty input

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
-import cosine_search
+from search_methods.cosine_search import search_documents
+from utils.utils import read_data
 
 
 #Initialize Flask instance
 app = Flask(__name__)
 
-df = cosine_search.read_data()
+df = read_data()
 articles = df.to_dict('records')
+data = df["text"].tolist()
 
 @app.route('/')
 def index():
@@ -16,11 +18,9 @@ def index():
 def search():
     if request.method == 'POST':
         query = request.form["query"]
-        data = cosine_search.read_data()["text"].tolist()
-        result = cosine_search.search_documents(query, data)
+        if query.strip() == "":
+            return index()
+        result = search_documents(query, data)
         result = [r[1] for r in result]
         result = [articles[r] for r in result]
         return render_template('index.html', articles=result)
-
-
-

befee2b10acce6d1460dbf0ebd08bc351fa58524
oiva-johannes <72695556+oiva-johannes@users.noreply.github.com>
2024-03-07 12:58:59 +0200

    create func

--- utils/dates_delete.py
+++ utils/dates_delete.py
@@ -1,20 +1,20 @@
 from datetime import datetime, timedelta
 from utils.utils import read_data, write_data
 
+def filter_dates():
+    df = read_data()
+    articles = df.to_dict('records')
+    rm = []
+    delta = timedelta(weeks=2)
+    dt_current = datetime.now()
 
-df = read_data()
-articles = df.to_dict('records')
-rm = []
-delta = timedelta(weeks=2)
-dt_current = datetime.now()
+    for i in range(len(articles)):
+        date = articles[i]['date']
+        dt_object = datetime.strptime(date, '%d.%m.%Y')
+        difference = dt_current - dt_object
+        if difference > delta:
+            rm.append(i)
 
-for i in range(len(articles)):
-    date = articles[i]['date']
-    dt_object = datetime.strptime(date, '%d.%m.%Y')
-    difference = dt_current - dt_object
-    if difference > delta:
-        rm.append(i)
-
-print(rm)
-df.drop(rm,axis=0,inplace=True) # remove news articles that are older than 2 weeks
-write_data(df)
+    print(rm)
+    df.drop(rm,axis=0,inplace=True) # remove news articles that are older than 2 weeks
+    write_data(df)
